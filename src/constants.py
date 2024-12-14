892dd26aa3deeb9c27865514cd11cd668c1aed11
aysieelf <aysel.karadayi.a63@learn.telerikacademy.com>
2024-12-14 16:48:15 +0200

    fix: x and o colors

--- src/constants.py
+++ src/constants.py
@@ -5,12 +5,12 @@ CELL_SIZE = WINDOW_SIZE // GRID_SIZE  # size of each cell
 LINE_WIDTH = 4  # width of the lines that form the grid
 
 X_PADDING = 30  # padding from the cell edges for the X symbol
-X_WIDTH = 10  # width of the X symbol
-X_COLOR = (0, 0, 0)  # color of the X symbol
+X_WIDTH = 7  # width of the X symbol
+X_COLOR = (108, 151, 133)  # color of the X symbol
 
 O_PADDING = 25  # padding from the cell edges for the O symbol
-O_WIDTH = 7  # width of the O symbol
-O_COLOR = (0, 0, 0)  # color of the O symbol
+O_WIDTH = 5  # width of the O symbol
+O_COLOR = (184, 111, 80)  # color of the O symbol
 
 
 # COLORS -----------------------------------------------------------------------
