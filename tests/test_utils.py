467d4ca91dcdfdcacd823f2399b23c20a8d28bfb
aysieelf <aysel.karadayi.a63@learn.telerikacademy.com>
2024-12-21 12:26:53 +0200

    test: Add unit tests for utils.py

---
+++ tests/test_utils.py
@@ -0,0 +1,14 @@
+import unittest
+from src.utils import get_cell_from_mouse
+
+class UtilsShould(unittest.TestCase):
+    def test_getCellFromMouse_returnsRowCol(self):
+        self.assertEqual((0, 0), get_cell_from_mouse((0, 0)))
+        self.assertEqual((0, 1), get_cell_from_mouse((100, 0)))
+        self.assertEqual((0, 2), get_cell_from_mouse((200, 0)))
+        self.assertEqual((1, 0), get_cell_from_mouse((0, 100)))
+        self.assertEqual((1, 1), get_cell_from_mouse((100, 100)))
+        self.assertEqual((1, 2), get_cell_from_mouse((200, 100)))
+        self.assertEqual((2, 0), get_cell_from_mouse((0, 200)))
+        self.assertEqual((2, 1), get_cell_from_mouse((100, 200)))
+        self.assertEqual((2, 2), get_cell_from_mouse((200, 200)))
